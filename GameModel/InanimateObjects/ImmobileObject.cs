f9036f4c7503481125784e07b198b227c1c91156
ninnikukawaii <ninnikukawaii@ya.ru>
2018-04-29 13:43:28 +0500

    Работа с декорами

--- GameModel/InanimateObjects/ImmobileObject.cs
+++ GameModel/InanimateObjects/ImmobileObject.cs
@@ -12,6 +12,34 @@ namespace GameThief.GameModel.InanimateObjects
 
         public SortedSet<IDecor> Decors = new SortedSet<IDecor>();
 
+        public void AddDecor(IDecor decor)
+        {
+            IsSolid = IsSolid || decor.IsSolid();
+            IsTransparent = IsTransparent || decor.IsTransparent();
+            NoiseInsulation += decor.GetNoiseInsulation();
+            Decors.Add(decor);
+        }
+
+        public void RemoveDecor(IDecor decor)
+        {
+            Decors.Remove(decor);
+            NoiseInsulation -= decor.GetNoiseInsulation();
+
+            if (decor.IsSolid())
+            {
+                IsSolid = false;
+                foreach (var dec in Decors)
+                    IsSolid = IsSolid || dec.IsSolid();
+            }
+
+            if (!decor.IsTransparent())
+            {
+                IsTransparent = true;
+                foreach (var dec in Decors)
+                    IsTransparent = IsTransparent || dec.IsTransparent();
+            }
+        }
+
         public void Interact(ICreature creature)
         {
             var firstItem = Decors.FirstOrDefault();
