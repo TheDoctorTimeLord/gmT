43c4bc18c40119a7089ae0ed65cfa7cd2764cee4
ninnikukawaii <ninnikukawaii@ya.ru>
2018-05-04 03:12:59 +0500

    Тесты

--- Tests/ObjectsContainer_Tests.cs
+++ Tests/ObjectsContainer_Tests.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameThief.GameModel.Enums;
 using GameThief.GameModel.ImmobileObjects;
 using GameThief.GameModel.ImmobileObjects.Decors;
 using GameThief.GameModel.ImmobileObjects.Items;
+using GameThief.GameModel.Managers;
+using GameThief.GameModel.MobileObjects;
+using GameThief.GameModel.ServiceClasses;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
@@ -64,6 +69,34 @@ namespace GameThief.Tests
             var container = new ObjectsContainer();
             container.AddDecor(new Carpet());
             Assert.False(container.IsSolid);
+        }
+
+        //[Test]
+        //public void TestInteractWithRemoval()
+        //{
+        //    SampleMapSetter.SetSampleMap(3, 3);
+        //    var container = MapManager.Map[1, 1].ObjectContainer;
+        //    container.AddDecor(new Table());
+        //    container.AddDecor(new Vase());
+        //    Assert.True(container.ShowDecor() is Vase);
+        //    var player = new Player(new InitializationMobileObject(new Point(0, 1), Direction.Right));
+        //    MobileObjectsManager.CreateCreature(player);
+        //    MobileObjectsManager.UpdateAnimates();
+        //    container.Interact(MapManager.Map[0,1].Creature);
+        //    Assert.True(container.ShowDecor() is Table);
+        //}
+
+        [Test]
+        public void TestInteractWithNoRemoval()
+        {
+            SampleMapSetter.SetSampleMap(3, 3);
+            var container = MapManager.Map[1, 1].ObjectContainer;
+            container.AddDecor(new Table());
+            Assert.True(container.ShowDecor() is Table);
+            var player = new Player(new InitializationMobileObject(new Point(0, 1), Direction.Right));
+            MapManager.AddCreatureToMap(player);
+            container.Interact(player);
+            Assert.True(container.ShowDecor() is Table);
         }
     }
 }
