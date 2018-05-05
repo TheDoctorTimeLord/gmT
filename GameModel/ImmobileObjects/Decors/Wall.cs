bd389917f754a73b42cc5359d9145535d1bf7e73
ninnikukawaii <ninnikukawaii@ya.ru>
2018-05-05 18:37:18 +0500

    Merge conflicts fixed

--- GameModel/ImmobileObjects/Decors/Wall.cs
+++ GameModel/ImmobileObjects/Decors/Wall.cs
@@ -10,6 +10,6 @@ namespace GameThief.GameModel.ImmobileObjects.Decors
 {
     public class Wall : ImmobileObject
     {
-        public Wall() : base(DecorType.Wall, 1, 2, true, true) { }
+        public Wall() : base(DecorType.Wall, 1, 20, true, true) { }
     }
 }
