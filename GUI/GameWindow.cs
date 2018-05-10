19bd5f30cb52166fc6e07b423cda11c9b40a82d1
Замотаев Фёдор <fedzam99@mail.ru>
2018-05-10 15:13:43 +0500

    МОЖНО ИГРАТЬ

--- GUI/GameWindow.cs
+++ GUI/GameWindow.cs
@@ -32,7 +32,7 @@ namespace GameThief.GUI
             foreach (var e in imagesDirectory.GetFiles("*.png"))
                 bitmaps[e.Name] = (Bitmap)Image.FromFile(e.FullName);
             var timer = new Timer();
-            timer.Interval = 15;
+            timer.Interval = 120;
             timer.Tick += TimerTick;
             timer.Start();
         }
@@ -44,17 +44,15 @@ namespace GameThief.GUI
             DoubleBuffered = true;
         }
 
-        //protected override void OnKeyDown(KeyEventArgs e)
-        //{
-        //    pressedKeys.Add(e.KeyCode);
-        //    Game.KeyPressed = e.KeyCode;
-        //}
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            GameState.KeyPressed = e.KeyCode;
+        }
 
-        //protected override void OnKeyUp(KeyEventArgs e)
-        //{
-        //    pressedKeys.Remove(e.KeyCode);
-        //    Game.KeyPressed = pressedKeys.Any() ? pressedKeys.Min() : Keys.None;
-        //}
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            GameState.KeyPressed = Keys.None;
+        }
 
         protected override void OnPaint(PaintEventArgs e)
         {
@@ -87,14 +85,7 @@ namespace GameThief.GUI
 
         private void TimerTick(object sender, EventArgs args)
         {
-            //if (tickCount == 0)
             gameState.UpdateState();
-            //foreach (var e in gameState.Animations)
-            //    e.Location = new Point(e.Location.X + 4 * e.Command.DeltaX, e.Location.Y + 4 * e.Command.DeltaY);
-            //if (tickCount == 7)
-            //    gameState.EndAct();
-            //tickCount++;
-            //if (tickCount == 8) tickCount = 0;
             Invalidate();
         }
 
